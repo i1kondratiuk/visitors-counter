1412de43b0d93f977c9e7fbff458de463c4955dc
Ivan Kondratiuk <ivan.kondratiuk@bp.com>
2020-05-03 15:55:46 +0300

    fix sql script

--- _sql/schema.sql
+++ _sql/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS `asset` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(18) unsigned zerofill NOT NULL AUTO_INCREMENT,
-  `name` varchar(256 NOT NULL,
-  `usarname` varchar(256 NOT NULL,
-  `password` varchar(256 NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `usarname` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `usarname_UNIQUE` (`usarname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
