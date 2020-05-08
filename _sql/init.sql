16e9f9455475a7530b027894971ec82e0a490458
Ivan Kondratiuk <i1kondratiuk@gmail.com>
2020-05-08 11:55:03 +0300

    add init.sql

--- _sql/init.sql
+++ _sql/init.sql
@@ -1,15 +1,18 @@
+CREATE DATABASE IF NOT EXISTS `vc`;
+
+USE `vc`;
+
 CREATE TABLE IF NOT EXISTS `user`
 (
-    `id`       int(18) unsigned zerofill NOT NULL AUTO_INCREMENT,
-    `name`     varchar(256)              NOT NULL,
-    `username` varchar(256)              NOT NULL,
-    `password` varchar(256)              NOT NULL,
+    `id`       int NOT NULL AUTO_INCREMENT,
+    `name`     varchar(256) NOT NULL,
+    `username` varchar(256) NOT NULL,
+    `password` varchar(256) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `id_UNIQUE` (`id`),
     UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+  DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `visit_log`
 (
@@ -21,5 +24,4 @@ CREATE TABLE IF NOT EXISTS `visit_log`
     PRIMARY KEY (`id`),
     UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+  DEFAULT CHARSET = utf8mb4;
